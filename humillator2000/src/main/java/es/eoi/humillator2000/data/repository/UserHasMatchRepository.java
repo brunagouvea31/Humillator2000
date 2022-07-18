fdaf4bc622cf77a1ce5922ed6a859e4edb42b555
C41STIAN <106078080+C41STIAN@users.noreply.github.com>
2022-07-18 20:04:38 +0200

    Sesión de trabajo 08/07

---
+++ humillator2000/src/main/java/es/eoi/humillator2000/data/repository/UserHasMatchRepository.java
@@ -0,0 +1,8 @@
+package es.eoi.humillator2000.data.repository;
+
+import es.eoi.humillator2000.data.entity.UserHasMatch;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserHasMatchRepository extends JpaRepository<UserHasMatch, Integer> {
+
+}
